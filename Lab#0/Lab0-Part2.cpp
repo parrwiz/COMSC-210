511099fb3fd7789d29cd9bb681d948f7bd8a8f64
Parwiz Khan <69708717+parrwiz@users.noreply.github.com>
2022-02-08 22:46:27 -0800

    Add files via upload

---
+++ Lab#0/Lab0-Part2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float area, base, height;
+    base = 3.0;
+    height = 4.0;
+
+    // Compute the area of the triangle and output it
+    area = (base * height) / 2.0;
+    cout << "The area of the triangle is: " << area << endl;
+
+    return 0;
+}
